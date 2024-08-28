b317c7936115be0298874383c391a1382384612e
nenoyy <saifutdinovaalbina1404@gmail.com>
2024-08-28 19:56:56 +0400

    Изменено название проекта TaskPhonebook

--- TaskPhonebook/obj/Debug/net6.0/ConsoleApp1.AssemblyInfo.cs
+++ TaskPhonebook/obj/Debug/net6.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25ed631b52e4f24ae33e358e6732c75e9b774544")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
