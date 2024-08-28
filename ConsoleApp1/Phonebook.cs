25ed631b52e4f24ae33e358e6732c75e9b774544
nenoyy <saifutdinovaalbina1404@gmail.com>
2024-08-28 19:53:03 +0400

    Добавлено решение задачи Phonebook

---
+++ ConsoleApp1/Phonebook.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Phonebook
+    {
+        private static List<Abonent> abonents = new List<Abonent>();
+
+        private static Phonebook instance;
+        private Phonebook()
+        {
+            ReadFile();
+        }
+
+        public void StorePhonebook()
+        {
+            var stringAbonents = "";
+            foreach (var abonent in abonents)
+            {
+                stringAbonents += $"{abonent.Name} {abonent.NumberPhone}\n";
+            }
+            File.WriteAllText("phonebook.txt", stringAbonents);
+        }
+
+        public static void ReadFile()
+        {
+            File.AppendAllText("phonebook.txt", "");
+            FileInfo fi1 = new FileInfo("phonebook.txt");
+            using (StreamReader sr = fi1.OpenText())
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    string[] person = s.Split(' ');
+                    Abonent abonent = new Abonent();
+                    abonent.Name = person[0];
+                    abonent.NumberPhone = int.Parse(person[1]);
+                    abonents.Add(abonent);
+                }
+            }
+        }
+
+        public static Phonebook getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Phonebook();
+            }
+            return instance;
+        }
+
+        public void AddAbonent(Abonent abonent)
+        {
+            if(abonents.Any(x => x.Name == abonent.Name || x.NumberPhone == abonent.NumberPhone))
+            {
+                Console.WriteLine("Такого абонента не существует");
+            }
+            else
+            {
+                abonents.Add(abonent);
+            }
+        }
+
+        public void RemoveAbonent(string name) 
+        {
+            Abonent remove = new Abonent();
+            foreach (var person in abonents)
+            {
+                if(person.Name == name)
+                {
+                    remove = person;
+                }
+            }
+            abonents.Remove(remove);
+        }
+
+        public Abonent SearchAbonent(long number)
+        {
+            foreach (var person in abonents)
+            {
+                if (person.NumberPhone == number)
+                {
+                    Console.WriteLine(person.Name + " " + person.NumberPhone);
+                    return person;
+                }
+            }
+            Console.WriteLine("Такого абонента не существует");
+            return null;
+        }
+
+        public Abonent SearchAbonent(string name)
+        {
+            foreach (var person in abonents)
+            {
+                if (person.Name == name)
+                {
+                    Console.WriteLine(person.Name + " " + person.NumberPhone);
+                    return person;
+                }
+            }
+            Console.WriteLine("Такого абонента не существует");
+            return null;
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var phonebook = Phonebook.getInstance();
+            bool check = true;
+            while (check)
+            {
+                Console.WriteLine("Нажмите соответствующую клавишу:\n" +
+                    "1. Добавить абонент\n" +
+                    "2. Удалить абонент\n" +
+                    "3. Поиск абонента по имени\n" +
+                    "4. Поиск абонента по номеру\n" +
+                    "5. Выход\n");
+
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.WriteLine("Введите имя абонента");
+                        string nameAbonent = Console.ReadLine();
+                        Console.WriteLine("Введите номер абонента");
+                        long numberAbonent = long.Parse(Console.ReadLine());
+
+                        var abonent = new Abonent
+                        {
+                            Name = nameAbonent,
+                            NumberPhone = numberAbonent
+                        };
+
+                        phonebook.AddAbonent(abonent);
+                        break;
+
+                    case ConsoleKey.D2:
+                        Console.WriteLine("Введите имя абонента");
+                        string nameRemove = Console.ReadLine();
+                        phonebook.RemoveAbonent(nameRemove); 
+                        break;
+
+                    case ConsoleKey.D3:
+                        Console.WriteLine("Введите имя абонента");
+                        string name = Console.ReadLine();
+                        phonebook.SearchAbonent(name);
+                        break;
+
+                    case ConsoleKey.D4:
+                        Console.WriteLine("Введите номер абонента");
+                        int number = int.Parse(Console.ReadLine());
+                        phonebook.SearchAbonent(number);
+                        break;
+
+                    case ConsoleKey.D5:
+                        phonebook.StorePhonebook();
+                        check = false;
+                        break;
+                }
+            }
+            
+        }
+    }
+}
