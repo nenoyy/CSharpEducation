242c63e4d39630b0efe3b6427b2d1d14231b5894
nenoyy <saifutdinovaalbina1404@gmail.com>
2024-09-28 22:44:19 +0400

    выполнено домашнее задание №5

---
+++ Task5/UserManagement/UserManagement/UserManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagement
+{
+  public class UserManager
+  {
+    List<User> users = new List<User>();
+
+    public void AddUser(User user)
+    {
+      foreach (var item in users) 
+      { 
+        if(item.Id == user.Id)
+        {
+          throw new UserAlreadyExistsException("Пользователь с таким ID уже добавлен");
+        }
+      }
+      users.Add(user);
+    }
+
+    public void RemoveUser(int id)
+    {
+      User user = null;
+      foreach (var item in users) 
+      { 
+        if(item.Id == id)
+        {
+          user = item;
+          break;
+        }
+      }
+      if (user is null)
+      {
+        throw new UserNotFoundException("Пользователь не найден");
+      }
+      users.Remove(user);
+    }
+
+    public User GetUser(int id)
+    {
+      foreach (var item in users)
+      {
+        if (item.Id == id)
+        {
+          return item;
+        }
+      }
+      throw new UserNotFoundException("Пользователь не найден");
+    }
+
+    public List<User> ListUsers() 
+    { 
+      return users;
+    }
+   }
+}
